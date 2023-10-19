f61a02f8e844e84635a4529b49ff19e1520c303c
Julio Cesar Zittei Dutra <julio.zittei96@gmail.com>
2023-10-18 23:52:47 -0300

    feat: add Footer component and refactor Header component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="opacity-7 bg-accent px-8 py-4 text-center text-[0.625rem]">
+      © 2023 Copyright <span className="font-semibold">FSW Store</span>
+    </footer>
+  );
+};
+
+export { Footer };
