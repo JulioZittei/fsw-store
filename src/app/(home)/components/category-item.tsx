a67fe21c499d0f2fb826e739db3af356427602c5
Julio Cesar Zittei Dutra <julio.zittei96@gmail.com>
2023-10-19 01:04:16 -0300

    feat: ensure user goes to category page on category item clickl

--- src/app/(home)/components/category-item.tsx
+++ src/app/(home)/components/category-item.tsx
@@ -1,4 +1,5 @@
 import { Badge } from "@/components/ui/badge";
+import { CATEGORY_ICON } from "@/constants/category-icon";
 import { Category } from "@prisma/client";
 import {
   Headphones,
@@ -8,28 +9,23 @@ import {
   Speaker,
   Square,
 } from "lucide-react";
+import Link from "next/link";
 
 interface CategoryItemProps {
   category: Category;
 }
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
-  const categoryIcons = {
-    keyboards: <Keyboard size={16} />,
-    mouses: <Mouse size={16} />,
-    monitors: <Monitor size={16} />,
-    headphones: <Headphones size={16} />,
-    mousepads: <Square size={16} />,
-    speakers: <Speaker size={16} />,
-  };
   return (
-    <Badge
-      variant="outline"
-      className="flex items-center justify-center gap-2 rounded-lg py-3"
-    >
-      {categoryIcons[category.slug as keyof typeof categoryIcons]}
-      <span className="text-xs font-semibold">{category.name}</span>
-    </Badge>
+    <Link href={`category/${category.slug}`}>
+      <Badge
+        variant="outline"
+        className="flex items-center justify-center gap-2 rounded-lg py-3"
+      >
+        {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
+        <span className="text-xs font-semibold">{category.name}</span>
+      </Badge>
+    </Link>
   );
 };
 
