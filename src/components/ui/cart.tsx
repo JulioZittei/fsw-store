3059510a4e241bf21cc34fc078fc279dcbe7eaef
Julio Cesar Zittei Dutra <julio.zittei96@gmail.com>
2023-10-24 14:29:41 -0300

    feat: ensure cart products can be scrolled

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -4,13 +4,15 @@ import { useContext } from "react";
 import { CartContext } from "@/providers/cart";
 import { CartItem } from "./cart-item";
 import { Separator } from "./separator";
+import { ScrollArea } from "./scroll-area";
+import { Button } from "./button";
 
 const Cart = () => {
   const { products, subTotal, totalPrice, totalDiscount } =
     useContext(CartContext);
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex h-full flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
@@ -19,17 +21,21 @@ const Cart = () => {
         Carrinho
       </Badge>
 
-      <ul className="flex flex-col gap-5">
-        {products.length > 0 ? (
-          products.map((product) => (
-            <li key={product.id}>
-              <CartItem product={product} />
-            </li>
-          ))
-        ) : (
-          <p className="text-center font-semibold">Carrinho vazio =[</p>
-        )}
-      </ul>
+      <div className="flex h-full max-h-full flex-col gap-5 overflow-hidden">
+        <ScrollArea className="h-full">
+          <ul className="flex h-full flex-col gap-8">
+            {products.length > 0 ? (
+              products.map((product) => (
+                <li key={product.id}>
+                  <CartItem product={product} />
+                </li>
+              ))
+            ) : (
+              <p className="text-center font-semibold">Carrinho vazio =[</p>
+            )}
+          </ul>
+        </ScrollArea>
+      </div>
 
       <div className="flex flex-col gap-3">
         <Separator />
@@ -74,6 +80,13 @@ const Cart = () => {
             })}
           </p>
         </div>
+
+        <Button
+          className="mt-7 font-bold uppercase"
+          // onClick={handleFinishPurchaseClick}
+        >
+          Finalizar compra
+        </Button>
       </div>
     </div>
   );
